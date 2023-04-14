54e19b2cb8013651e2a9d5876232733bc718720e
vanshjain-22 <74652502+vanshjain-22@users.noreply.github.com>
2023-04-14 23:44:28 +0530

    1

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const serverless = require('serverless-http');
+
+require('dotenv').config();
+
+
+const app=express();
+
+//EJS
+
+app.set('view engine','ejs');
+
+//body-parser
+app.use(express.urlencoded({ extended: true }));
+
+
+//adding static files like css
+app.use('/',express.static(__dirname+"/assets"))
+
+
+app.get('/',function(req,res){
+    res.render('index');
+})
+app.get('/notes',function(req,res){
+    res.render('notes');
+})
+app.get('/analyzer',function(req,res){
+    res.render('analyzer');
+});
+
+//var PORT = process.env.PORT || 5000;
+
+//app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(process.env.port || 5000 , () =>{
+    console.log('Server is running');
+});
